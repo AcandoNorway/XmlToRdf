d8bbbc47c3b6f50a72ee1e8c356200156cd044c7
Håvard Ottestad <hmottestad@gmail.com>
2016-05-30 20:19:26 +0200

    faster list and string handling in jena

--- src/main/java/no/acando/xmltordf/ObjectBasedSaxHandlerJena.java
+++ src/main/java/no/acando/xmltordf/ObjectBasedSaxHandlerJena.java
@@ -17,23 +17,27 @@ limitations under the License.
 package no.acando.xmltordf;
 
 import org.apache.jena.atlas.io.NullOutputStream;
+import org.apache.jena.datatypes.xsd.XSDDatatype;
 import org.apache.jena.graph.*;
 import org.apache.jena.query.Dataset;
 import org.apache.jena.query.DatasetFactory;
-import org.apache.jena.rdf.model.*;
+import org.apache.jena.rdf.model.ResourceFactory;
 import org.apache.jena.sparql.util.NodeFactoryExtra;
 import org.openrdf.model.IRI;
+import org.openrdf.model.vocabulary.RDF;
 import org.xml.sax.SAXException;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ArrayBlockingQueue;
-import java.util.stream.Collectors;
 
 
 public class ObjectBasedSaxHandlerJena extends ObjectBasedSaxHandler {
 
 
+    private static final Node RDF_REST = NodeFactory.createURI(RDF.REST.toString());
+    private static final Node RDF_FIRST = NodeFactory.createURI(RDF.FIRST.toString());
+    private static final Node RDF_NIL = NodeFactory.createURI(RDF.NIL.toString());
     private Graph g;
 
     Dataset dataset;
@@ -144,7 +148,9 @@ public class ObjectBasedSaxHandlerJena extends ObjectBasedSaxHandler {
             subjectNode = NodeFactory.createBlankNode(subject);
         }
 
-        Node literal = NodeFactory.createLiteral(objectLiteral, "", false);
+        //Node literal = NodeFactory.createLiteral(objectLiteral, null, false);
+        Node literal = NodeFactory.createLiteral(objectLiteral, XSDDatatype.XSDstring);
+
         Triple triple = new Triple(subjectNode, predicateNode, literal);
         try {
             queue.put(triple);
@@ -231,49 +237,86 @@ public class ObjectBasedSaxHandlerJena extends ObjectBasedSaxHandler {
             subjectNode = NodeFactory.createBlankNode(subject);
         }
 
+        final Node[] head = new Node[1];
+        final Node[] temporaryNode = new Node[1];
 
-        Model defaultModel = ModelFactory.createDefaultModel();
 
-        List<RDFNode> collect = mixedContent.stream().map(content -> {
-            if (content instanceof String) {
-                String objectLiteral = (String) content;
+        mixedContent
+            .stream()
+            .map(content -> {
+                if (content instanceof String) {
 
-                return defaultModel.createLiteral(objectLiteral);
+                    String objectLiteral = (String) content;
+                    return NodeFactory.createLiteral(objectLiteral, XSDDatatype.XSDstring);
 
-            } else if (content instanceof Element) {
-                Element objectElement = (Element) content;
-                if (!objectElement.getUri().startsWith("_:")) {
-                    return defaultModel.createResource(objectElement.getUri());
+                } else if (content instanceof Element) {
+
+                    Element objectElement = (Element) content;
+
+                    if (objectElement.getUri().startsWith("_:")) {
+                        return NodeFactory.createBlankNode(objectElement.getUri());
+                    } else {
+                        return NodeFactory.createURI(objectElement.getUri());
+                    }
 
                 } else {
-                    return defaultModel.createResource(new AnonId(objectElement.getUri()));
+                    throw new IllegalStateException("Unknown type of: " + content.getClass().toString());
                 }
 
-            } else {
-                throw new IllegalStateException("Unknown type of: " + content.getClass().toString());
-            }
+            })
+            .forEachOrdered(value -> {
+                Node blankNode = NodeFactory.createBlankNode();
 
-        }).collect(Collectors.toList());
+                if (head[0] == null) {
+                    head[0] = blankNode;
+                    try {
+                        queue.put(new Triple(head[0], RDF_FIRST, value));
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
 
-        RDFList list = defaultModel.createList( collect.toArray(new RDFNode[collect.size()]));
+                } else {
+                    try {
+                        queue.put(new Triple(temporaryNode[0], RDF_REST, blankNode));
+                        queue.put(new Triple(blankNode, RDF_FIRST, value));
+
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+
+                temporaryNode[0] = blankNode;
 
 
-        Model model = list.getModel();
-        StmtIterator stmtIterator = model.listStatements();
-        while(stmtIterator.hasNext()){
-            Statement next = stmtIterator.next();
-            try {
-                queue.put(next.asTriple());
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
+            });
 
         try {
-            queue.put(new Triple(subjectNode, predicateNode, list.asNode()));
+            queue.put(new Triple(temporaryNode[0], RDF_REST, RDF_NIL));
+            queue.put(new Triple(subjectNode, predicateNode, head[0]));
+
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+
+
+//
+//        try {
+//            queue.put(new Triple(subjectNode, predicateNode, head[0]));
+//
+//
+//            queue.put(new Triple(head[0], RDF_FIRST, collect.get(0)));
+//            Node parent = head[0];
+//            for (int i = 1; i < collect.size(); i++) {
+//                Node blankNode = NodeFactory.createBlankNode();
+//                parent = blankNode;
+//            }
+//
+//
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+
 
         return null;
     }
@@ -283,11 +326,8 @@ public class ObjectBasedSaxHandlerJena extends ObjectBasedSaxHandler {
 
         notDone = false;
 
-
-        queue.add(EndOfFileTriple);
-
-
         try {
+            queue.put(EndOfFileTriple);
             jenaThread.join();
         } catch (InterruptedException e) {
             e.printStackTrace();
