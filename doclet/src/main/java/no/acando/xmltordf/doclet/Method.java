41318b6ecf7475d6c4151203ba66b67b7d176e3d
Håvard Ottestad <hmottestad@gmail.com>
2016-06-21 16:09:42 +0200

    added copyright

--- doclet/src/main/java/no/acando/xmltordf/doclet/Method.java
+++ doclet/src/main/java/no/acando/xmltordf/doclet/Method.java
@@ -1,19 +1,32 @@
+/*
+Copyright 2016 ACANDO AS
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ */
+
 package no.acando.xmltordf.doclet;
 
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Created by havardottestad on 16/06/16.
- */
-public class Method {
+ class Method {
 
 	String name;
 	String description;
 
 	List<Example> examples = new ArrayList<>();
 
-	public void addExample(Example currentExample) {
+	 void addExample(Example currentExample) {
 		examples.add(currentExample);
 	}
 }
