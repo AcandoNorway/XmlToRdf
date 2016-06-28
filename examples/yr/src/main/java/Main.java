2864f6aafc0b1c6da3069cd6768db05b317b6357
Håvard M. Ottestad <havard.m.ottestad@posccaesar.org>
2016-06-28 21:15:05 +0200

    added copyright

--- examples/yr/src/main/java/Main.java
+++ examples/yr/src/main/java/Main.java
@@ -1,3 +1,19 @@
+/*
+Copyright 2016 ACANDO AS
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ */
+
 import no.acando.xmltordf.Builder;
 import org.apache.commons.io.IOUtils;
 import org.apache.jena.query.Dataset;
@@ -9,9 +25,7 @@ import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 
-/**
- * Created by havardottestad on 27/06/16.
- */
+
 public class Main {
 
 	public static void main(String[] args) throws URISyntaxException, IOException, ParserConfigurationException, SAXException {
@@ -22,7 +36,7 @@ public class Main {
 
 		Builder.AdvancedJena builder = Builder.getAdvancedBuilderJena()
 				.convertComplexElementsWithOnlyAttributesAndSimpleTypeChildrenToPredicate(true)
-				.convertComplexElementsWithOnlyAttributesToPredicates(true)
+				.convertComplexElementsWithOnlyAttributesToPredicate(true)
 				.setBaseNamespace("http://www.yr.no/", Builder.AppliesTo.bothElementsAndAttributes)
 				.renameElement("http://www.yr.no/link", "http://www.yr.no/Link")
 				.renameElement("http://www.yr.no/weatherstation", "http://www.yr.no/Weatherstation")
